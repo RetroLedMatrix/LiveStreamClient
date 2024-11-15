337ac3d527804b49b3b763a64659fe3279e250f8
Marco Prescher <mpr8243@students.fhv.at>
2024-11-15 21:02:36 +0100

    added basic http server

---
+++ src/api/rest/server.cpp
@@ -0,0 +1,31 @@
+//
+// Created by marco on 15/11/2024.
+//
+
+#include "server.h"
+#include "../../../includes/httplib.h"
+#include <iostream>
+
+void start_server() {
+    httplib::Server svr;
+
+    std::string server_url = "http://localhost:" + std::to_string(REST_SERVER_PORT);
+
+    svr.Get("/", [server_url](const httplib::Request &, httplib::Response &res) {
+        res.set_content("Server is running at " + server_url, "text/html");
+    });
+
+    svr.Get("/hello", [](const httplib::Request &, httplib::Response &res) {
+        res.set_content("Hello, World!", "text/plain");
+    });
+
+    svr.Post("/echo", [](const httplib::Request &req, httplib::Response &res) {
+        res.set_content(req.body, "text/plain");
+    });
+
+    std::cout << "Server is running at " << server_url << std::endl;
+
+    if (!svr.listen("127.0.0.1", REST_SERVER_PORT)) {
+        std::cerr << "Error: Failed to start the server on port " << REST_SERVER_PORT << "!" << std::endl;
+    }
+}
---
+++ src/api/rest/server.h
@@ -0,0 +1,13 @@
+//
+// Created by marco on 15/11/2024.
+//
+
+#ifndef SERVER_H
+#define SERVER_H
+#include <cstdint>
+
+constexpr static uint16_t REST_SERVER_PORT = 8090;
+
+void start_server();
+
+#endif //SERVER_H
