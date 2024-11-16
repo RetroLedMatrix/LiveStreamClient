6844524996b53936b537cb25307d521bb8245892
f0olofaT0ok <willo4man2002@gmail.com>
2024-11-16 16:02:51 +0100

    Implemented threading for the WebsocketClient

--- main.cpp
+++ main.cpp
@@ -2,8 +2,13 @@
 // Created by Katie on 15/11/2024.
 //
 
+#include <functional>
+#include <iostream>
+
 #include "includes/api/WebSocketClient.h"
 #include "includes/BrainsBehindScreenshot.h"
+#include <thread>
+
 
 int main() {
     // fingers crossed lol
@@ -11,7 +16,17 @@ int main() {
     auto bmp = screencapture.CaptureScreenBitmap();
 
 
+
     WebSocketClient client("localhost", "8080", "/publisher/matrixMarco");
-    client.run();
+    //threading
+    std::thread client_thread(std::bind(&WebSocketClient::run, &client));
+
+    for(int i = 0; i < 100; i++) {
+        std::cout << "testing" << i << std::endl;
+    }
+    client_thread.join();
+
+
+    //client.run();
     return 0;
 }
