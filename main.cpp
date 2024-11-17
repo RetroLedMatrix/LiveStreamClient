a8e2782a438046e4bb5f087f86a40b624e9c44ad
Marco Prescher <mpr8243@students.fhv.at>
2024-11-17 11:09:52 +0100

    Use localhost

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 #include "includes/api/WebSocketClient.h"
 
 int main() {
-    WebSocketClient client("192.168.1.102", "8080", "/publisher/matrix");
-    // WebSocketClient client("localhost", "8080", "/publisher/matrix-test");
+    // WebSocketClient client("192.168.1.102", "8080", "/publisher/matrix");
+    WebSocketClient client("localhost", "8080", "/publisher/matrix-test");
     client.run();
     return 0;
 }
